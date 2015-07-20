c36bf82c26b3377df948045277c3864c8c1993a6
midhul varma <midhul.v@gmail.com>
2015-07-20 11:44:39 +0530

    Update explorer.pl

--- a-star/explorer.pl
+++ a-star/explorer.pl
@@ -2,16 +2,19 @@
 %% Uses the A* search algorithm to explore the grid
 :-dynamic pea_handler/3.
 
-pea_handler(guid, (IP, Port), main) :- 
-    %% If goal has been reached execute PTA
-    (current_predicate(is_goal/0) -> writeln('Reached Goal!'), agent_execute(pta, (IP, Port), pta_handler) ;
+%% If goal has been reached
+pea_handler(guid, (IP, Port), main) :- current_predicate(is_goal/0),
+    writeln('Reached Goal!'), agent_execute(pta, (IP, Port), pta_handler).
 
+% If goal has not been reached
+pea_handler(guid, (IP, Port), main) :- not(current_predicate(is_goal/0)),
     %% If the node is jammed do nothing
     (current_predicate(jammed/0) -> true ;
-    %% Make a list of neighbours
-    findall((N,Dir), (neighbour(N,Dir), N\=null), L),
-    %% Handle neighbours in the list
-    pea_handler(guid, (IP,Port), handle_neighbours(L))
+    % Otherwise
+        %% Make a list of neighbours
+        findall((N,Dir), (neighbour(N,Dir), N\=null), L),
+        %% Handle neighbours in the list
+        pea_handler(guid, (IP,Port), handle_neighbours(L))
     ),
 
     %% Remove node from fringe and add to closed.
@@ -21,30 +24,33 @@ pea_handler(guid, (IP, Port), main) :-
     findall(Val, priority(guid, _, Val), PriList),
     min_list(PriList, Min),
     priority(guid, Next, Min),
-    agent_move(guid, Next)
+    agent_move(guid, Next).
     
-    ).
 
 %% Pedicate to handle neighbours
 %% Empty list (base case)
 pea_handler(guid, (_IP,_Port), handle_neighbours([])).
 %% Recursively handle neighbours if non empty list
-pea_handler(guid, (IP, Port), handle_neighbours([(N,Dir)|T])) :-
-    %% Handle the head of the list.
-    (closed(guid, N) -> true ;
-        %% find new priority value
-        dist(G),heuristic(H),
-        ((Dir=north;Dir=east) -> NewVal is G+1+H-1; NewVal is G+1+H+1),
-        (priority(guid, N, OldVal) ->
-            %% Already in the fringe
-            (NewVal<OldVal ->
-                retract(priority(guid, N, OldVal)), assert(priority(guid, N, NewVal)),
-                %% Update the neighbour
-                NewDist is G+1,
-                pea_handler(guid, (IP, Port), update(N, NewDist))
+
+% If top of list is already in closed then continue to next
+pea_handler(guid, (IP, Port), handle_neighbours([(N,Dir)|T])) :- closed(guid, N),
+    pea_handler(guid, (IP, Port), handle_neighbours(T)).
+    
+% 
+pea_handler(guid, (IP, Port), handle_neighbours([(N,Dir)|T])) :- not(closed(guid, N)),
+     %% find new priority value
+    dist(G),heuristic(H),
+    ((Dir=north;Dir=east) -> NewVal is G+1+H-1; NewVal is G+1+H+1),
+    (priority(guid, N, OldVal) ->
+        %% Already in the fringe
+        (NewVal<OldVal ->
+            retract(priority(guid, N, OldVal)), assert(priority(guid, N, NewVal)),
+            %% Update the neighbour
+            NewDist is G+1,
+            pea_handler(guid, (IP, Port), update(N, NewDist))
             ;
-                %% Do nothing
-                true 
+            %% Do nothing
+            true 
             )
          ;
             %% Not in fringe
@@ -53,10 +59,7 @@ pea_handler(guid, (IP, Port), handle_neighbours([(N,Dir)|T])) :-
             Dist is G+1,
             pea_handler(guid, (IP, Port), update(N, Dist))
 
-        )
-
-    ),
-    %% recursion
+        ),
     pea_handler(guid, (IP, Port), handle_neighbours(T)).
 
 %% Predicate to update neighbours
