7ff525a7a8b457b53231a6cb86b578db91da62b8
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-07-20 11:35:03 +0530

    added basics

--- basics/agent.pl
+++ basics/agent.pl
@@ -2,8 +2,9 @@
 
 %% handler predicates are used to handle agents in various situations
 %% all mobile agent handlers must be declared as dynamic predicates
-:-dynamic foo_handler/3.
 
+:- dynamic foo_handler/3.
+:- dynamic payload_number/2.
 %% structure of the agent handler must be as follows
 %% The first parameter must be `guid` This will be replaced by the actual name of
 %% the agent when one is created
@@ -14,14 +15,21 @@
 foo_handler(guid, (IP, Port), main) :- 
     writeln('Hello world, I am foo!').
 
+payload_number(guid,X):-
+	X=3;
+	X=2;
+	X=1.
+
 %% Steps to create an agent
 create :- 
     %% get platform details
     get_platform_details(IP, Port),
     agent_create(foo, (IP, Port), foo_handler),
-
+    add_payload(foo,[(payload_number,2)]) ,
     %% We give the agent our token, so it can freely move in and out of this
     %% platform
     add_token(foo, [9595]).
-
+kill:-
+	get_platform_details(IP,Port),
+	agent_kill(foo).
 
