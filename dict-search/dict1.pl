8bec6a542287f68f32fcd5fc4e774168a7b113b3
Nikhil Alamanda <ant.nikhil96@gmail.com>
2015-07-20 10:25:52 +0530

    Update dict1.pl

--- dict-search/dict1.pl
+++ dict-search/dict1.pl
@@ -8,6 +8,14 @@ location(localhost, 7001).
 %% Words in the dictionary
 %% format - word('word', 'meaning')
 word(ingenious, 'cleverly and originally devised and well suited to its purpose').
+word(epoch,'A period of time in history or a person\'s life').
+word(pertrichor,'Pleasant smell after rain').
+word(epiphany,'A moment of sudden realisation').
+word(oblivion,'A state of bieng unaware of what is happening around you').
+word(limerence,'State of being infatuated with another person').
+word(nefarious,'Wicked and dispecable').
+word(ethereal,'Extremely light and delicate').
+word(sonorous,'Melodious and pleasant to hear').
 word(hypothetical, 'based on or serving as a hypothesis').
 word(gigantic, 'of very great size or extent; huge or enormous').
 word(superfluous, 'unnecessary, especially through being more than enough').
