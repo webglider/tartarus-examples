1480bcf12d6271c3128ebffc88d463ab76abdece
Midhul Varma <midhul.v@gmail.com>
2015-07-22 11:28:13 +0530

    modify tsp

--- Travelling Salesman Problem/salesman.pl
+++ Travelling Salesman Problem/salesman.pl
@@ -3,15 +3,26 @@
 :-dynamic checklist/2.
 
 salesman_handler(guid, (_IP, _Port), main) :- 
+    %% Attcah console is used to cleanly display output and take input
+    attach_console,
     %% When the agent reaches a city
     %% Send log message
     name(Name), atom_concat('Reached ', Name, Msg),
     send_log(guid, Msg),
+    writeln(Msg),
 
     %% If there are unvisited cities
     %% If all cities have been visited, prolog
     %% will backtrack to next clause definition
     %% Find the nearest unvisited neighbour
+    %% Print all possible options
+    
+    writeln('Options:'),
+    writeln('--------------------------'),
+    writeln('City ----> Price'),
+    writeln('---------------------------'),
+    
+    forall((checklist(guid, City), price(City, Price)), (write(City),write('---->'), writeln(Price))),
     findall(Price, (checklist(guid, City), price(City, Price)), L),
     min_list(L, Min),
     price(Destination, Min),
@@ -19,6 +30,8 @@ salesman_handler(guid, (_IP, _Port), main) :-
     retract(checklist(guid, Name)),
     %% Move to Destination
     location(Destination, Location),
+    writeln('Press ENTER to continue'),
+    get_char(_),
     agent_move(guid, Location),!.
     %% red cut used to ensure it does not backtrack
     
