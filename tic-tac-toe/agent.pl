42f1b381414b5101156a6dc047f0a622f81cf0be
Midhul Varma <midhul.v@gmail.com>
2015-08-04 02:52:57 +0530

    correct spellings

--- tic-tac-toe/agent.pl
+++ tic-tac-toe/agent.pl
@@ -1,6 +1,6 @@
-:-dynamic refree_handler/3.
+:-dynamic referee_handler/3.
 
-%% Handler for the refree mobile agent.
+%% Handler for the referee mobile agent.
 
 %% The agent carries the `board` predicate as payload
 %% board(guid, Row, Col, Symbol)
@@ -10,81 +10,81 @@
 %% Symbol can be `x` or `o` or `_`
 
 %% Prints a single row of the board on a line
-refree_handler(guid, _, print_row(R)) :-
+referee_handler(guid, _, print_row(R)) :-
     nl, write(R),
     board(guid,R,1,X),write(' '),write(X),
     board(guid,R,2,Y),write('  '),write(Y),
     board(guid,R,3,Z),write('  '),write(Z),nl.
 
 %% Prints the present state of the game board
-refree_handler(guid, _, print_board) :- 
+referee_handler(guid, _, print_board) :- 
     writeln('----------'),
     writeln('  1  2  3'),
-    refree_handler(guid, nothing, print_row(a)),
-    refree_handler(guid, nothing, print_row(b)),
-    refree_handler(guid, nothing, print_row(c)),
+    referee_handler(guid, nothing, print_row(a)),
+    referee_handler(guid, nothing, print_row(b)),
+    referee_handler(guid, nothing, print_row(c)),
     writeln('----------').
 
 %% Predicate to find symbol of present player
-%% refree_handler(guid, _, symbols(This, Other))
-%% `This` is unfied with symbol of player on present platform
+%% referee_handler(guid, _, symbols(This, Other))
+%% `This` is unified with symbol of player on present platform
 %% `Other` is unified with symbol of player on other platform
-refree_handler(guid, _, symbols(x,o)) :- current_predicate(home/0).
-refree_handler(guid, _, symbols(o,x)) :- not(current_predicate(home/0)).
+referee_handler(guid, _, symbols(x,o)) :- current_predicate(home/0).
+referee_handler(guid, _, symbols(o,x)) :- not(current_predicate(home/0)).
 
 
 %% Main Handler
 %% This is executed as soon as the agent reaches a platform
-refree_handler(guid, (IP, Port), main) :- 
+referee_handler(guid, (IP, Port), main) :- 
     attach_console, 
     %% (this ensures that nothing input and output from user are
     %% taken cleanly from a separate console)
     %% Print present board state
-    refree_handler(guid, (IP, Port), print_board),
-    refree_handler(guid, nothing, symbols(Sym, Osym)),
+    referee_handler(guid, (IP, Port), print_board),
+    referee_handler(guid, nothing, symbols(Sym, Osym)),
 
     %% Has somebody won?
     %% fail ensures that execution does not continue if the game has terminated
-    (refree_handler(guid, (IP, Port), has_won(Sym)) ->
+    (referee_handler(guid, (IP, Port), has_won(Sym)) ->
         writeln('YOU HAVE WON THE GAME'),read(_),!,fail   
     ;true),
-    (refree_handler(guid, (IP, Port), has_won(Osym)) ->
+    (referee_handler(guid, (IP, Port), has_won(Osym)) ->
         writeln('YOU HAVE LOST THE GAME'),read(_),!,fail   
     ;true),
 
     %% Take input from player
-    refree_handler(guid, (IP,Port), take_input).
+    referee_handler(guid, (IP,Port), take_input).
 
 
 %% This predicate takes input of the move the player wants to make
-refree_handler(guid, (IP,Port), take_input) :- 
+referee_handler(guid, (IP,Port), take_input) :- 
     writeln('Its your turn now!'),
     writeln('Please put a fullstop after your input'),
     write('Enter row [a or b or c]: '), read(Row),
     write('Enter column [1 or 2 or 3]: '), read(Col),
     %% try to make move
-    refree_handler(guid, (IP, Port), make_move(Row,Col)).
+    referee_handler(guid, (IP, Port), make_move(Row,Col)).
 
 %% This predicate tries to make the move which the user has input
 %% If the move is not valid as for input again
-refree_handler(guid, (IP, Port), make_move(X,Y)) :- not(board(guid, X, Y, '_')),
+referee_handler(guid, (IP, Port), make_move(X,Y)) :- not(board(guid, X, Y, '_')),
         writeln('Refree: Invalid move!'),
-        refree_handler(guid, (IP,Port), take_input).
+        referee_handler(guid, (IP,Port), take_input).
 
 %% If the move is valid
-refree_handler(guid, (IP, Port), make_move(X,Y)) :- board(guid, X, Y, '_'),
-    refree_handler(guid, (IP, Port), symbols(Sym, Osym)),
+referee_handler(guid, (IP, Port), make_move(X,Y)) :- board(guid, X, Y, '_'),
+    referee_handler(guid, (IP, Port), symbols(Sym, Osym)),
     write('Refree: You made a move at location '), 
     write(X),write(','),write(Y),nl,
     retract(board(guid, X,Y, '_')),
     assert(board(guid,X,Y,Sym)),
-    refree_handler(guid, (IP, Port), print_board),
+    referee_handler(guid, (IP, Port), print_board),
 
     %% Has somebody won?
-    (refree_handler(guid, (IP, Port), has_won(Sym)) ->
+    (referee_handler(guid, (IP, Port), has_won(Sym)) ->
         writeln('YOU HAVE WON THE GAME')   
     ;true),
-    (refree_handler(guid, (IP, Port), has_won(Osym)) ->
+    (referee_handler(guid, (IP, Port), has_won(Osym)) ->
         writeln('YOU HAVE LOST THE GAME')   
     ;true),
 
@@ -94,17 +94,17 @@ refree_handler(guid, (IP, Port), make_move(X,Y)) :- board(guid, X, Y, '_'),
     agent_move(guid, (Destination_IP, Destination_Port)).
 
 %% Tic tac toe logic
-refree_handler(guid, _, has_won(S)) :- 
+referee_handler(guid, _, has_won(S)) :- 
     %% Horizontal?
     board(guid, Row, One, S), board(guid, Row, Two, S), 
     board(guid, Row, Three, S), One \= Two, Two \= Three, One \= Three.
-refree_handler(guid, _, has_won(S)) :- 
+referee_handler(guid, _, has_won(S)) :- 
     %% Vertical?
     board(guid, One, Col, S), board(guid, Two, Col, S), 
     board(guid, Three, Col, S), One \= Two, Two \= Three, One \= Three.
-refree_handler(guid, _, has_won(S)) :-
+referee_handler(guid, _, has_won(S)) :-
     %% Main Diagonal?
     board(guid, b, 2, S), board(guid, a, 1, S), board(guid, c, 3, S).
-refree_handler(guid, _, has_won(S)) :-
+referee_handler(guid, _, has_won(S)) :-
     %% Off Diagonal?
     board(guid, b, 2, S), board(guid, c, 1, S), board(guid, a, 3, S).
--- tic-tac-toe/play.pl
+++ tic-tac-toe/play.pl
@@ -17,11 +17,11 @@ board(guid, c, 3, '_').
 %% Refree agent is initialized
 start :-
     get_platform_details(IP, Port), 
-    agent_create(refree, (IP, Port), refree_handler),
-    add_token(refree, [9595]),
+    agent_create(referee, (IP, Port), referee_handler),
+    add_token(referee, [9595]),
     %% Add the board payload
-    add_payload(refree, [(board, 4)]),
+    add_payload(referee, [(board, 4)]),
     assert(home),
     writeln('Remember to set play_with!'),
 
-    agent_execute(refree, (IP, Port), refree_handler).
+    agent_execute(referee, (IP, Port), referee_handler).
